94bd92bd274dd9b8dee0c8efc405276050b83e91
Chris Gitmed <cgitmed@gmail.com>
2020-11-05 16:45:24 -0800

    Adjust right and up key listeners so that if car is on, the car moves right and up respectively

--- main.js
+++ main.js
@@ -12,15 +12,28 @@ function moveRight() {
   $car.style.left = car.xPosition + 'px';
 }
 
+function moveUp() {
+  car.yPosition -= 2;
+  $car.style.top = car.yPosition + 'px';
+}
+
 document.addEventListener('keydown', function (event) {
   if (event.code === 'ArrowLeft') {
     $car.className = 'rotate-left';
 
   } else if (event.code === 'ArrowRight') {
     $car.className = 'rotate-right';
+    if (!carIsOff) {
+      clearInterval(intervalID);
+      intervalID = setInterval(moveRight, 16);
+    }
 
   } else if (event.code === 'ArrowUp') {
     $car.className = 'rotate-up';
+    if (!carIsOff) {
+      clearInterval(intervalID);
+      intervalID = setInterval(moveUp, 16);
+    }
 
   } else if (event.code === 'ArrowDown') {
     $car.className = 'rotate-down';
